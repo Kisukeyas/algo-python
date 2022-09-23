de081e5bc5fef101b51aa3c1608005181a0b7c50
Kisukeyas <kisuke.yas@gmail.com>
2022-09-24 01:40:42 +0900

    2022/9/23

---
+++ Array-search/N9.py
@@ -0,0 +1,11 @@
+# データを受け取る
+N = int(input())
+A = list(map(int,input().split()))
+
+# 線形探索
+for i in range(1,10):
+    cnt = 0
+    for t in range(1,N):
+        if i == A[t]:
+            cnt += 1
+            print(cnt)
