d8489da446c556ab7e83be474059d80656cf698e
Kisukeyas <kisuke.yas@gmail.com>
2022-09-24 08:11:02 +0900

    2022/9/24

--- Array-search/N10.py
+++ Array-search/N10.py
@@ -1,11 +1,20 @@
 # データを受け取る
 N = int(input())
 A = list(map(int,input().split()))
+B = []
+Max = 1
 
 # 線形探索
 for i in range(1,10):
     cnt = 0
-    for t in range(1,N):
+    for t in range(0,N):
         if i == A[t]:
             cnt += 1
-            print(cnt)
+    B.append(cnt)
+for l in range(0,9):
+    if Max < B[l]:
+        Max = B[l]
+        Answer = B.index(B[l])+1
+
+# 答えを出力する
+print(Answer)
--- Array-search/N9.py
+++ Array-search/N9.py
@@ -5,7 +5,7 @@ A = list(map(int,input().split()))
 # 線形探索
 for i in range(1,10):
     cnt = 0
-    for t in range(1,N):
+    for t in range(0,N):
         if i == A[t]:
             cnt += 1
-            print(cnt)
+    print(cnt)
