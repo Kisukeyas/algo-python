6a8e306bc95ef3cf1cd7f2b1b41f8c37c33c6aa2
Kisukeyas <kisuke.yas@gmail.com>
2022-10-17 00:26:36 +0900

    2022/10/17

--- full search/N5.py
+++ full search/N5.py
@@ -1,12 +1,11 @@
 # データを受け取る
 N = int(input())
 # 数値の全探索
-M = N+1
 
-for i in range(1,M):
-    if i % 3 == 0:
-        if i % 5 == 0:
-            print('FizzBuzz')
+for i in range(1,N+1):
+    if (i % 5 == 0 and i % 3 == 0):
+        print('FizzBuzz')    
+    elif i % 3 == 0:
         print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
