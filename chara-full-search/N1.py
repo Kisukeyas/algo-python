73ef5e80fe851154d15765142d1079b5025bd881
Kisukeyas <kisuke.yas@gmail.com>
2022-10-17 23:14:54 +0900

    20221017

---
+++ chara-full-search/N1.py
@@ -0,0 +1,24 @@
+# S = input()
+# c = input()
+
+# if c in S:
+#     print('Yes')
+# else:
+#     print('No')
+
+# データを受け取る
+S = input()
+c = input()
+
+# S の長さを取得する
+N = len(S)
+
+# 線形探索
+flag = False
+for i in range(N):
+    if S[i] == c:
+        flag = True
+
+# 答えを出力する
+if flag: print("Yes")
+else: print("No")
