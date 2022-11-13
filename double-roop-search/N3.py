2c3e4a4a0d8a392aff7d9fb53416fe511929f498
Kisukeyas <kisuke.yas@gmail.com>
2022-11-13 19:00:00 +0900

    20221113

---
+++ double-roop-search/N3.py
@@ -0,0 +1,16 @@
+# データを受け取る
+N, K = map(int, input().split())
+
+Ans = 0
+
+# 配列の全探索
+
+for i in range(1, N+1):
+    counter = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            counter += 1
+    if counter == K:
+        Ans += 1
+
+print(Ans)
